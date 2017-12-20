5932fbd6e8547d420d177c77d5a3dbda7f3847fb
BonLeung <654592397@qq.com>
2017-12-21 00:58:55 +0800

    commit

--- client/views/topic-list/index.jsx
+++ client/views/topic-list/index.jsx
@@ -2,17 +2,30 @@ import React from 'react'
 import { observer, inject } from 'mobx-react'
 import PropTypes from 'prop-types'
 
-@inject('appState') @observer
+import { AppState } from '../../store/app-state'
+
+@inject('appState')
+@observer
 export default class TopicList extends React.Component {
   static propTypes = {
-    appState: PropTypes.isRequired,
+    appState: PropTypes.instanceOf(AppState).isRequired,
+  }
+  constructor(props) {
+    super(props)
+    this.changeName = this.changeName.bind(this)
   }
   componentDidMount() {
     // do something
   }
+  changeName(event) {
+    this.props.appState.changeName(event.target.value)
+  }
   render() {
     return (
-      <div>{this.props.appState.msg}</div>
+      <div>
+        <input type="text" onChange={this.changeName} />
+        <span>{this.props.appState.msg}</span>
+      </div>
     )
   }
 }
